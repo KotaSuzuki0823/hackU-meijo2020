4f48bed2deeae79e1f4c55309301cbb0c2eb82cd
KotaSuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2020-10-26 16:28:56 +0900

    ユーザ情報取得機能を実装

--- Backend/server.py
+++ Backend/server.py
@@ -7,6 +7,9 @@ import hashlib
 strage_key = os.getenv('AZURE_STRAGE_KEY')
 strage_name = os.getenv('AZURE_STRAGE_NAME')
 
+AZURE_TABLENAME_USER = 'user'
+AZURE_TABLENAME_HELP = 'help'
+
 api = Flask(__name__)
 
 @api.route('/user/registration', methods=['POST'])
@@ -18,21 +21,21 @@ def user_registration():
     try:
         # クエストからユーザ情報を抽出し辞書型に変換
         userdata = {
-            'PartitionKey': hashlib.sha256(request.form["user_id"]).hexdigest(),  # 必須のキー情報
-            'RowKey': request.form["user_id"],        # 必須のキー情報
+            'PartitionKey': hashlib.sha256(request.form["user_id"]).hexdigest(),  # 必須のキー情報,user_idをSHA256でハッシュ化
+            'RowKey': request.form["user_id"],        # 必須のキー情報，ユーザID
             'user_name': request.form["user_name"],
             'user_nic': request.form["user_nic"],
             'gender': request.form["gender"],
-            'user_age': request.form["user_age"],
+            'user_age': int(request.form["user_age"]),
             'places': request.form["places"],
             'user_tel': request.form["user_tel"],
             'can_do': request.form["can_do"]
         }
 
         # DBへユーザ情報を追加
-        TableService.insert_or_replace_entity('user', userdata)   
+        TableService.insert_or_replace_entity(AZURE_TABLENAME_USER, userdata)   
     except:
-        abort(404)
+        abort(500)
 
 @api.route('/user/get', methods=['GET'])
 def get_user():
@@ -41,20 +44,40 @@ def get_user():
     :return:
     '''
     try:
-        # DBへユーザ情報を追加
+        # クエリ文字列から検索するエリアを指定
+        # http://ocalhost:3000/user/get?area=aichi&num=1
+        user_area = request.args.get('area')
+
+        # 辞書表示に使うインデックス
+        number = request.args.get('num') - 1
+
+        # テーブルからエリア条件に一致するユーザを取得
+        userlist = TableService.query_entities(
+            table_name=AZURE_TABLENAME_USER,
+            places=user_area
+        )
+
+        if number < len(userlist):
+            user = userlist[number]
+
+            result = {
+            "result":True,
+            "data":{
+                "userId":user.userId,
+                "user_name":user.name,
+                "user_gender":user.gender,
+                "user_age":user_age
+                }
+            }
+
+        else:
+            print("これ以上はありません．")
+            result = {
+                "result":False
+            }
 
     except:
-        abort(404)
-    
-    result = {
-        "result":True,
-        "data":{
-            "userId":user.userId,
-            "name":user.name,
-            "caption":user.caption,
-            "old":int(user.old)
-            }
-        }
+        abort(500)
 
     return make_response(jsonify(result))
 
@@ -77,10 +100,17 @@ def help_offer_registration():
     :return:
     '''
     try:
+        helpdata = {
+            'PartitionKey': hashlib.sha256(request.form["user_id"]).hexdigest(),  # 必須のキー情報,user_idをSHA256でハッシュ化
+            'RowKey': request.form["user_id"],        # 必須のキー情報，ユーザID
+            'user_name': request.form["outline"],
+            'user_nic': request.form["detail"],
+            'can_do': request.form["can_do"]
+        }
         # お助け情報の追加
-        TableService.insert_or_replace_entity('otasuke', otasukedata)  
+        TableService.insert_or_replace_entity(AZURE_TABLENAME_HELP, helpdata)  
     except:
-        abort(404)
+        abort(500)
 
 @api.route('/help_offer/get', methods=['GET'])
 def get_help_offer():
@@ -92,7 +122,7 @@ def get_help_offer():
         #DBへユーザ情報を追加
 
     except:
-        abort(404)
+        abort(500)
 
 
 @api.errorhandler(404)
