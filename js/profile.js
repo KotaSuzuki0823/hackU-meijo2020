32c3bab52b6eb46c6ca895115beb197eb70d259e
yuto-ema <180442021@ccmailg.meijo-u.ac.jp>
2020-11-04 10:11:50 +0900

    修正版(動かなかったら泣きたい)

--- js/profile.js
+++ js/profile.js
@@ -1,29 +1,17 @@
-/* sumple
-
-$("user").submit(function(event){
-// stop form from submitting normally
-    event.preventDefault();
-//get some values from elements on the pages
-    var $form=$(this),
-    term=$form.find("input[name='s]").val,
-    url=$form.attr("action");
-//send the data using post
-    var posting=$.post(url,{s:term});
-//put the data using post
-    posting.done(function(data){
-        var content=$(data).find("#content");
-        $("#result").empty().append(content);
-    });
-});
-
-*/  
-
+/* postの送信の仕方 
 $("user").submit(function(event){
     event.preventDefault();
     
     $.post('https://httpbin.org/post',$('form').serialize())
 
-    .done(function(data){
-        console.log(data.form);
-    })
+})
+
+*/
+
+$("#user").submit(function(event){
+    /* event.preventDefault(); */
+    /* alert($('form').serialize()); */
+    var test=$('form').serialize();
+    alert(test);
+
 })
