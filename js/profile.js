3807f8080afe7ab4544a9383f3a7e489909d93ff
yuto-ema <180442021@ccmailg.meijo-u.ac.jp>
2020-11-08 00:52:19 +0900

    進捗

--- js/profile.js
+++ js/profile.js
@@ -1,19 +1,44 @@
-/* postの送信の仕方 
-$("#user").submit(function(event){
+/*
+$("form").submit(function(event){
     event.preventDefault();
-    
-    $.post('https://httpbin.org/post',$('form').serialize())
 
-})
+    var url="https://localhost:5000/user/registration";
+    $.post(url,$('form').serialize())
 
+});
 */
 
-$("#user").submit(function(event){
-    /* event.preventDefault(); */
-    /* alert($('form').serialize()); */
-    var url="";
-    var test=$('form').serialize();
-    $.post('https://correliv.azurewebsites.net/user/registration',$('form').serialize())
-    alert(test);
+/*
+$("form").submit(function (event) {
 
-})
+    var url = "https://localhost:5000/user/registration";
+    var test = 'これはテストデータです';
+
+    const res = await axios.post('http://localhost:5000/user/registration', {
+        id: 123,
+        name: 'Yamada Tarou'
+    }
+    )
+    alert('完了');
+});
+*/
+var url = "http://localhost:5000/user/registration";
+
+
+$("form").submit(function getdataFetch() {
+    var response=$('form').serialize();
+    fetch(url, {
+        mode: 'cors'
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (body) {
+            console.log(body.fetch)
+            document.getElementById("resultFetch").innerText += body.fetch;
+        })
+        .catch(function (error) {
+            console.log(error)
+            document.getElementById("resultFetch").innerText = 'ERR';
+        });
+});
